e9be242f2f01eb8e86e02a8c9454534c78d6f824
KIMYEIM <kyaeim@gmail.com>
2021-07-21 13:38:21 +0900

    Header 완성

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -3,8 +3,24 @@ import styled from '@emotion/styled';
 import Logo from 'components/Main/Logo';
 
 const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 6;
   max-width: 100%;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.7);
+  box-shadow: 0px 5px 3px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+  //border-bottom: solid 7px #233660;
+  /*
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 32px;
+  font-weight: 900;
+  color: #233660;
+  */
 `;
 
 const Header: FunctionComponent = function () {
--- src/components/Main/Logo.tsx
+++ src/components/Main/Logo.tsx
@@ -4,15 +4,13 @@ import logoImage from '../../../contents/hiarc_logo.svg';
 
 const LogoWrapper = styled.img`
   background-color: black;
-  width: 60px;
-  height: 60px;
+  width: 45px;
+  height: 45px;
   border-radius: 50%;
-  margin: 33px auto 27px 52px;
+  margin: 18px 0 10px 52px;
 
   @media (max-width: 768px) {
-    width: 45px;
-    height: 45px;
-    margin: 20px auto 15px 25px;
+    margin-left: 28px;
   }
 `;
 
