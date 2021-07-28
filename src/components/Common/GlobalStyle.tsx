a2597e13eaad7a47cc48d9b467cccaa2e33424e4
KIMYEIM <kyaeim@gmail.com>
2021-07-28 15:29:22 +0900

    Header, Icpc 수정

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -2,7 +2,8 @@ import React, { FunctionComponent } from 'react';
 import { Global, css } from '@emotion/react';
 
 const defaultStyle = css`
-  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap');  
+  
   * {
     padding: 0;
     margin: 0;
