5a5ad907c1102208695039ca0420d4f3081d791a
huiji0315 <huiji0315@gmail.com>
2021-08-04 03:39:19 +0900

    communication 반응형 디자인 수정 및 MoveToDetail 컴포넌트 추가

--- src/components/Common/Description.tsx
+++ src/components/Common/Description.tsx
@@ -5,20 +5,25 @@ const DescriptionWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  color: #233660;
+  white-space: pre-wrap;
+  color: black;
   font-size: 30px;
   line-height: 2;
   text-align: center;
   margin: 20px 120px;
 
-  @media (max-width: 470px) {
+  @media (max-width: 600px) {
     font-size: 20px;
+    width: 320px;
+  }
+  @media (min-width: 1300px) {
+    margin-left: 310px;
   }
 `;
 
 const Description: FunctionComponent = function () {
   const aboutText =
-    'HI-ARC의 전반적인 커뮤니케이션은 Slack을 통해 이루어집니다. BOJ와 Codeforces 등의 알고리즘 문제부터, 학교 생활이나 개발 등의 타 분야에 대한 질문까지 HI-ARC Slack의 질문 채널에서 자유롭게 질문할 수 있습니다. 또한 매 학기 개강총회와 종강총회를 진행하여 한 학기 동안의 활동들을 돌아보는 자리를 가집니다.';
+    'HI-ARC의 전반적인 커뮤니케이션은 Slack을 통해 이루어집니다.\n BOJ와 Codeforces 등의\n 알고리즘 문제, 학교 생활이나 개발 등의 타 분야에 대한 질문까지\n HI-ARC Slack의 질문 채널에서\n 자유롭게 질문할 수 있습니다.\n 또한 매 학기 개강총회와 종강총회를 진행하여 한 학기 동안의 활동들을\n 돌아보는 자리를 가집니다.';
   return (
     <DescriptionWrapper>
       {aboutText}
--- src/pages/communication.tsx
+++ src/pages/communication.tsx
@@ -2,13 +2,15 @@ import React, { FunctionComponent } from 'react';
 import Template from 'components/Common/Template';
 import Description from 'components/Common/Description';
 import Post from 'components/Common/Post';
+import MoveToDetail from 'components/Common/MoveToDetail';
 
 const CommunicationPage: FunctionComponent = () => {
   return (
     <>
       <Template title='Communication'>
         <Description />
-        <Post />
+        <MoveToDetail/>
+        <Post />    
       </Template>
     </>
   );
