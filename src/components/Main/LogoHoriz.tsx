ab5793f7e0fcd5daf87420c4cd50f718432e31e1
KIMYEIM <kyaeim@gmail.com>
2021-08-12 16:22:45 +0900

    Header 스크롤 감지 추가, 그림자 제거

--- src/components/Main/LogoHoriz.tsx
+++ src/components/Main/LogoHoriz.tsx
@@ -5,7 +5,7 @@ import Horiz from '../../../contents/hiarc_logo_horiz.png';
 
 const Logo = styled.img`
     width: 130px;
-    filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.3));
+    //filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.3));
     margin-top: 5px;
     margin-left: 40px;
     cursor: pointer;
