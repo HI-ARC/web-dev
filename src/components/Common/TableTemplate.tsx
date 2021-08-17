71adecfca08861b754edbacb39da31eb6dc2f0ac
yevin <kimpika8541@naver.com>
2021-08-18 00:40:34 +0900

    Records 수정
    - 헤더 볼드처리&색 추가 
    - records 설명 추가
    - 연도별 보기 & 대회 기록 위치 수정 (우측으로 이동)
    - 반응형 페이지 대회제목과 표 겹침 수정 (확인필요)

--- src/components/Common/TableTemplate.tsx
+++ src/components/Common/TableTemplate.tsx
@@ -1,22 +1,66 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const Member = styled.div`
+
+const Default = styled.div`
+th{
+  font-size:21px;
+  height: 6vh;
+  width:20.5vw;
+  background-color:#233660;
+  color: white;
+  padding-top:0.5vh;
+ }
+  td{width: 20.5vw; }
   @media (max-width: 750px) {
     display: none;
   }
 `;
 
-const TableTemplate = ({ prize, team, member }) => {
+const Reactive = styled.div`
+th{
+  height: 5vh;
+  width:45vw;
+  background-color:#233660;
+  color: white;
+ }
+td{ width: 45vw;}
+@media (min-width: 750px) {
+  display: none;
+}
+`;
+
+export const TableTemplate = ({ prize, team, member }) => {
   return (
     <table>
-      <td width="18%">{prize}</td>
-      <td width="62%">{team}</td>
-      <Member>
-        <td width="38%">{member}</td>
-      </Member>
+      <Default>
+      <td>{prize}</td>
+      <td>{team}</td>
+      <td>{member}</td>
+      </Default>
+      <Reactive>
+      <td>{prize}</td>
+      <td>{team}</td>
+      </Reactive>
     </table>
+   
   );
 };
 
 export default TableTemplate;
+
+export const CreateHeader = () => {
+  return (
+    <table>
+      <Default>
+      <th>🏆</th>
+      <th>TEAM</th>
+      <th>MEMBER</th>
+      </Default>
+      <Reactive>
+      <th>🏆</th>
+      <th>TEAM</th>
+      </Reactive>
+    </table>
+  );
+};
