43973152240652c40b1bd843291858b6666b9041
KIMYEIM <kyaeim@gmail.com>
2021-08-30 22:24:59 +0900

    친목 페이지 헤더 변경

---
+++ src/components/Main/Menu2.tsx
@@ -0,0 +1,38 @@
+import React, { FunctionComponent } from 'react';
+import { Link } from 'gatsby';
+import styled from '@emotion/styled';
+
+const MenuList = styled.div`
+  width: 40%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  place-items: center;
+  margin-right: 20px;
+  margin-left: auto;
+  font-size: 16px;
+  font-weight: 700;
+
+  @media (min-width: 750px) {
+    width: 300px;
+    font-size: 18px;
+  }
+
+  @media (max-width: 600px) {
+    width: 230px;
+    font-size: 13px;
+  }
+`;
+
+const Menu2: FunctionComponent = function () {
+  return (
+    <>
+      <MenuList>
+        <Link to="/records2021">수상</Link>
+        <Link to="/study/">스터디</Link>
+        <Link to="/communication">친목</Link>
+      </MenuList>
+    </>
+  );
+};
+
+export default Menu2;
